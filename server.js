76260ca1d1b07982c6391ae1b6f6d3bb9457ec2b
Ankur Rawat <ankurrawat620@gmail.com>
2023-02-17 23:17:37 +0530

    Routs

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get("/",  (req, res) => {
     res.send("<h1>Hello</h1>");
 })
 
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1>");
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>about</h1>");
+})
+
+app.get("/hobbies", (req, res) => {
+    res.send("<h1>Hobbies</h1>")
+})
+
 app.listen(3000, function (){
         console.log("Server Started On Port 3000")
 });
